90387a4cff700dad8608ad39a35c0db65ce5eeb3
alonhouber <73168537+alonhouber@users.noreply.github.com>
2020-12-13 13:50:40 +0200

    Update main.c

--- Factori/main.c
+++ Factori/main.c
@@ -6,6 +6,30 @@ typedef struct list {
 	list* next;
 }list;
 
+list* New__List(int number)
+{
+	list* new_list = (list*) malloc(sizeof(list));
+	new_list->number = number;
+	return new_list;
+}
+
+list* Add__ToList(list* head, int number)
+{
+	if (head == NULL)
+	{
+		head = New__List(number);
+		return head;
+	}
+	list* last_num;
+	last_num = head;
+	while (last_num->next != NULL)
+	{
+		last_num = last_num->next;
+	}
+	last_num->next = New__List(number);
+	return head;
+}
+
 list* Get__PrimeFactors(int number)
 {
 	list* p_prime_numbers_list = NULL;
