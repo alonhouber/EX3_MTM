5d60daece5d41cfdbb09465fb92246577f7246a3
alonhouber <73168537+alonhouber@users.noreply.github.com>
2020-12-13 13:45:16 +0200

    added list

--- Factori/main.c
+++ Factori/main.c
@@ -1,4 +1,34 @@
+#include <stdio.h>
 
+typedef struct list
+{
+	int number;
+	list* next;
+}list;
+
+list* New__List(int number)
+{
+	list* new_list = (list*) malloc(sizeof(list));
+	new_list->number = number;
+	return new_list;
+}
+
+list* Add__ToList(list* head, int number)
+{
+	if (head == NULL)
+	{
+		head = New__List(number);
+		return head;
+	}
+	list* last_num;
+	last_num = head;
+	while (last_num->next != NULL)
+	{
+		last_num = last_num->next;
+	}
+	last_num->next = New__List(number);
+	return head;
+}
 
 int main(int argc, char* argv[])
 {
