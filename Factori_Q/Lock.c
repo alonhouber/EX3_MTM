f838bf4d45fdb6a9a0838ce9c813301f74cecc4b
dvirbz <58605853+dvirbz@users.noreply.github.com>
2020-12-19 14:43:36 +0200

    Create Lock.c

---
+++ Factori_Q/Lock.c
@@ -0,0 +1,44 @@
+#include <Windows.h>
+#define WAIT_TIME 5000
+#include <stdio.h>
+#include <stdbool.h>
+typedef struct Lock {
+	HANDLE read_lock;
+	HANDLE write_lock;
+}Lock;
+
+Lock* New__Lock(int number_of_threads)
+{
+	Lock* my_lock = (Lock*)malloc(sizeof(Lock));
+	if (my_lock == NULL) {
+		printf("MEMORY_ALLOCATION_FAILED");
+		return NULL;
+	}
+	my_lock->read_lock = CreateSemaphoreA(NULL, 0, number_of_threads, NULL);
+	if (my_lock->read_lock == NULL){
+		return NULL;
+	}
+	my_lock->write_lock = CreateSemaphoreA(NULL, 0, 1, NULL);
+	if (my_lock->write_lock == NULL) {
+		return NULL;
+	}
+	return my_lock;
+}
+
+bool Read__Lock(Lock* my_Lock) {
+	DWORD wait_code;
+	wait_code = WaitForSingleObject(my_Lock->read_lock, WAIT_TIME);
+	if (WAIT_OBJECT_0 != wait_code) {		
+		return  false;
+	}
+	return true;
+}
+
+bool Read__Release(Lock* my_Lock) {
+	DWORD wait_code;
+	wait_code = WaitForSingleObject(my_Lock->read_lock, WAIT_TIME);
+	if (WAIT_OBJECT_0 != wait_code) {
+		return  false;
+	}
+	return true;
+}
