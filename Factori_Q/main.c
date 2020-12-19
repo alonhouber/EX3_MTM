3f39c40724d49ce51f34ec96fb3a496c72a5d84c
alonhouber <73168537+alonhouber@users.noreply.github.com>
2020-12-19 21:06:39 +0200

    new header list

--- Factori_Q/main.c
+++ Factori_Q/main.c
@@ -7,105 +7,21 @@
 #include "file_func.h"
 #include "Queue.h"
 #include "Lock.h"
+#include "List.h"
 
-#define DECIMAL_BASE (int)10
+
 #define READ_ONE_CHAR 1
 #define WAIT_TIME 20000
 #define MISSION_FILE_NAME_ARGUMENT 1
 #define PRIORITY_FILE_NAME_ARGUMENT 2
 #define MISSIONS_NUM_ARGUMET 3
 #define THREADS_NUM_ARGUMET 4
-#define START_OF_LINE_LEN 30
-#define COMMA_AND_SPACE_LEN 2
 #define WAIT_TIME_READ_LOCK 5000
 #define WAIT_TIME_WRITE_LOCK 5000
+#define DECIMAL_BASE (int)10
+#define START_OF_LINE_LEN 30
+#define COMMA_AND_SPACE_LEN 2
 
-typedef struct list_t {
-	int number;
-	struct list_t* next;
-}list;
-list* New__List(int number)
-{
-	list* new_list = (list*)malloc(sizeof(list));
-	if (new_list != NULL)
-	{
-		new_list->number = number;
-		new_list->next = NULL;
-	}
-	return new_list;
-}
-list* Add__ToList(list* head, int number)
-{
-	if (head == NULL)
-	{
-		head = New__List(number);
-		return head;
-	}
-	list* last_num;
-	last_num = head;
-	while (last_num->next != NULL)
-	{
-		last_num = last_num->next;
-	}
-	last_num->next = New__List(number);
-	return head;
-}
-int get_len_of_num(int num)
-{
-	int counter = 0;
-	while (num > 0)
-	{
-		num = num / DECIMAL_BASE;
-		counter++;
-	}
-	return counter;
-}
-
-int Get__line_list_length(list* head, int num)
-{
-	list* current_pos = head;
-	int len = START_OF_LINE_LEN + get_len_of_num(num);
-	if (head != NULL)
-	{
-		while (current_pos->next != NULL)
-		{
-			len = len + COMMA_AND_SPACE_LEN + get_len_of_num(current_pos->number);
-			current_pos = current_pos->next;
-		}
-		len = len + get_len_of_num(current_pos->number);
-	}
-	return len;
-}
-
-char* Print__List(list* head, int number, char* list_format_string, int memory_size)
-{
-	list* temp_head = head;
-	snprintf(list_format_string, memory_size, "The prime factors of %d are: ", number);
-	char* temp_string = list_format_string;
-	if (temp_head != NULL)
-	{
-		while (temp_head->next != NULL)
-		{
-			temp_string = list_format_string;
-			snprintf(list_format_string, memory_size, "%s%d, ", temp_string, temp_head->number);
-			temp_head = temp_head->next;
-		}
-		temp_string = list_format_string;
-		snprintf(list_format_string, memory_size, "%s%d", temp_string, temp_head->number);
-	}
-	snprintf(list_format_string, memory_size, "%s\r\n", temp_string);
-	return list_format_string;
-}
-void Free__List(list* head)
-{
-	list* temp = head;
-	while (head != NULL)
-	{
-		head = head->next;
-		free(temp);
-		temp = head;
-	}
-}
 list* Get__PrimeFactors(int number)
 {
 	list* p_prime_numbers_head = NULL;
